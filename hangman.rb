a8469a33be2dc7f09a1e3dd4df4951e47cbbc707
jthagerman <jthagerma@gmail.com>
2021-01-18 20:16:03 -0500

    initial commit

---
+++ hangman.rb
@@ -0,0 +1,16 @@
+wordlist = '5desk.txt'
+wordArray = []
+longestword = ''
+
+contents = File.open(wordlist)
+contents.each do |word|
+    if word.length > longestword.length
+        longestword = word
+    end
+    if(word.length > 5) && (word.length < 13)
+       
+        wordArray.push(word)
+    end
+end
+puts wordArray.length
+puts longestword
